3092146ec48857b77b8fc3aa744cacb3466258c4
Elif Savaş <98321352+elifsavas@users.noreply.github.com>
2022-11-15 17:03:58 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int n ;
+
+        Scanner input = new Scanner(System.in) ;
+        System.out.print("Kaç Satırlı Bir Üçgen İstiyorsunuz? : ");
+        n = input.nextInt();
+
+        for (int i = n ; i>= 1 ; i--) {
+            for (int j = 1 ; j <= n-i ; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1 ; k<= (2*i) - 1 ; k++) {
+
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+
     }
 }
